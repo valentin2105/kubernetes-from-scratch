97b48cbc0cb30af1739db8b4bee78ffd02361ead
Valentin Ouvrard <valentin.ouvrard@nautile.sarl>
2023-07-12 17:37:49 +1100

    ingress

--- run/9-ingress.sh
+++ run/9-ingress.sh
@@ -3,5 +3,3 @@ helm repo add traefik https://traefik.github.io/charts
 helm install traefik traefik/traefik \
               --set ports.web.nodePort=30080 \
               --set ports.websecure.nodePort=30443
-
-kubectl apply -f ingress.yaml
